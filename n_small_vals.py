6673a560a663be99946f058b43c081efec508e9d
siddhu-1002 <65851990+siddhu-1002@users.noreply.github.com>
2020-08-18 09:20:26 +0530

    Update n_small_vals.py

--- n_small_vals.py
+++ n_small_vals.py
@@ -1,15 +1,14 @@
-# Taking input from user about how many values to be added into list
-m = int(input("How many elements do you want to add to your list : "))
-# Created an empty list to add the input input values from the user
-numbers = []
+# Program to print n small values of the list
+
+m = int(input("How many elements do you want to add to your list : "))        # Taking input from user about how many values to be added into list
+numbers = []                                                                  # Created an empty list to add the input input values from the user
+
 for term in range(1,m+1):
     element = int(input("Enter the element of the list : "))
     numbers.append(element)
+
 dup_num = numbers.copy()
-
-# Taking input from user about how many minimum values to be printed
-n = int(input("How many small values do you want to print : "))
-
+n = int(input("How many small values do you want to print : "))               # Taking input from user about how many minimum values to be printed
 
 for i in range(1, n+1):
     low1 = min(dup_num)
